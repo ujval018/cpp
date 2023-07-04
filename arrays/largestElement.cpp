64729280556cbce9815eb50036ab955c621e1fc9
ujval018 <77689691+ujval018@users.noreply.github.com>
2023-07-04 17:20:45 +0530

    find the largest element of an array

--- arrays/largestElement.cpp
+++ arrays/largestElement.cpp
@@ -1,10 +1,10 @@
-//to find largest element of an array
+//to find the largest element of an array
 #include <iostream>
 using namespace std;
 
 int main(){
           int A[5]={1,3,9,7,13};
-          int m=0;
+          int m=A[0];
 
           for (int x:A)
           {
