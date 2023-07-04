fbedade9b3e2cf682cf369ba2b582b973cd501c4
ujval018 <77689691+ujval018@users.noreply.github.com>
2023-07-04 16:53:16 +0530

    Update array.cpp

--- arrays/array.cpp
+++ arrays/array.cpp
@@ -1,8 +1,27 @@
+//array initialization take the values and print using for loop and for each loop
 #include <iostream>
 using namespace std;
 
 int main(){
-          cout<<"check";
+          int A[5];
+
+          cout<<"enter array "<<endl;
+          for (int i = 0; i < 5; i++)
+          {
+            cin>>A[i];
+          }
+          cout<<"the array is:- ";
+
+          for (int i = 0; i < 5; i++)
+          {
+            cout<<A[i]<<" ";
+          }
+          cout<<endl<<"arrays using each loop :- ";
+          for (int x:A)
+          {
+            cout<<x<<" ";
+          }
+          
 
   
 return 0;
