02bdda5061e3efa2640e1a8146cd7c32225305a0
ujval018 <77689691+ujval018@users.noreply.github.com>
2023-07-07 12:31:21 +0530

    2d array initalization

---
+++ arrays/2darray.cpp
@@ -0,0 +1,29 @@
+/*additon of 2d array/ matrix*/
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int A[2][3]={1,2,3,4,5,6};
+  int B[2][3]={{6,7,8},{9,10,11}};
+  int C[2][3];
+  for (int i = 0; i < 2; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      C[i][j]=A[i][j]+B[i][j];
+    }
+    
+  }
+  for (int i = 0; i < 2; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      cout<<C[i][j]<<" ";
+    }cout<<endl;
+    
+  }
+  
+
+  return 0;
+}
