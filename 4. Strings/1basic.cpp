162a98b30cce5c409ee35318e24596d338816f47
ujval018 <77689691+ujval018@users.noreply.github.com>
2023-07-10 13:53:18 +0530

    basic of string

--- 4. Strings/1basic.cpp
+++ 4. Strings/1basic.cpp
@@ -6,7 +6,10 @@ int main(){
 
   char str[]="hello";
   cout<<str<<endl;
-  cout<<sizeof str;
+  char str1[6]={'h','e','l','l','o','\0'};
+  char str2[6]="Ujval"; //this way \0 automatically be entered in the array ---literal
+  char str3[]="hello"; // literal
+  //char *str="hello"; most compiler does not support
 
   return 0;
 }
