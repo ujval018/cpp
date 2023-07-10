80f6930d4679bbe19481d2e3170bcbc810740f1d
ujval018 <77689691+ujval018@users.noreply.github.com>
2023-07-10 16:08:30 +0530

    cin.get() take string input

---
+++ 4. Strings/2 ioStrings.cpp
@@ -0,0 +1,20 @@
+//take input string input and print it using cin.get function 
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  char str[100];
+  cout<<"what's your name?"<<endl;
+  cin.get(str,100);
+  cout<<"welcome to code world "<<str<<endl;
+  cin.ignore();//or just use getline function
+
+  char str1[100];
+  cout<<"what's your name again?"<<endl;
+  cin.get(str1,100);
+  cout<<"welcome to code world mr. "<<str1<<endl;
+  
+
+  return 0;
+}
