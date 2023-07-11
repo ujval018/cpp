25646f45c8d270000240fde0aefee5d0113763b2
ujval018 <77689691+ujval018@users.noreply.github.com>
2023-07-11 12:45:28 +0530

    strlen(), strcat, strncat, strcpy & strncpy

--- 4. Strings/3 cstring1.cpp
+++ 4. Strings/3 cstring1.cpp
@@ -1,20 +1,31 @@
-//strlen(), strcat and strncat
+//strlen(), strcat, strncat, strcpy, strncpy
 #include <iostream>
 #include<cstring>
 
 using namespace std;
 
 int main (){
+  //strcat use 
   char A[20]="Hello";
   char B[10]="RamRam";
   cout<<A<<" jii "<<B<<endl;  
   strcat(A,B);
   cout<<A<<endl;
+  //strncat use
   char a[20]="Hello";
   char b[10]="RamRam";
   strncat(a,b,3);
   cout<<a<<endl;  
-  cout<<"length of string A is "<<strlen(A);
+  //strlen use
+  cout<<"length of string A is "<<strlen(A)<<endl;
+  //strcpy use
+  char str1[20];
+  strcpy(str1,A);
+  cout<<str1<<endl;
+  //strncpy use
+  char str2[20];
+  strncpy(str2,B,3);
+  cout<<str2<<endl;
 
   return 0;
 }
