f8ca24ef1e83c2b00b3aa87fae393214664584b7
ujval018 <77689691+ujval018@users.noreply.github.com>
2023-07-12 11:14:22 +0530

    string class basics

---
+++ 4. Strings/5.stringclass.cpp
@@ -0,0 +1,17 @@
+//string class basics 
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main (){
+
+
+  string str1;//object str is created
+  cout<<"enter string"<<endl;
+  getline(cin,str1);
+  cout<<"entered string :- "<<str1<<endl;
+
+
+  return 0;
+}
