a26386923a9a6301212ebc0e490c1c321b23c137
ujval018 <77689691+ujval018@users.noreply.github.com>
2023-07-14 15:46:52 +0530

    copy substr find

---
+++ 4. Strings/8 stringfun.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (){
+  //string initilisation 
+  string s1="ram ram jii";
+  //string declaration
+  char s2[20];
+  //str.copy(from,length)
+  s1.copy(s2, s1.length());
+  cout<<s2<<endl;
+  //string declaration
+  char s3[20];
+  //str.copy(from,length)
+  s1.copy(s3, 3);
+  s3[3]='\0';//must be done else garbage value
+  cout<<s3<<endl;
+  //str.find() find the string/char
+  cout<<s1.find("jii")<<endl;  
+  cout<<s1.rfind('r')<<endl;
+  cout<<s1.find_first_of('a',3)<<endl;
+
+  //forms a new sub string 
+  cout<<s1.substr(3)<<endl;
+  cout<<s1.substr(3,4)<<endl;
+
+  return 0;
+}
